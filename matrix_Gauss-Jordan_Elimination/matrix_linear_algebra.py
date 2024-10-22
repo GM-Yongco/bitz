c0328e5f66b02c9081e23e6fe11a344331abe5de
Giulio Yongco <130813239+GM-Yongco@users.noreply.github.com>
2024-10-23 02:13:23 +0800

    renaming of variables

--- matrix_Gauss-Jordan_Elimination/matrix_linear_algebra.py
+++ matrix_Gauss-Jordan_Elimination/matrix_linear_algebra.py
@@ -40,78 +40,78 @@ def minimal_print_df(
 
 def row_swap(		
 		x:pd.DataFrame,
-		row_1:int = 1,
-		row_2:int = 1
+		row_a:int = 1,
+		row_b:int = 1
 		) -> None:
 			
-	row_1 = row_1 - 1
-	row_2 = row_2 - 1
+	row_a = row_a - 1
+	row_b = row_b - 1
 	# because math starts with 1 ;^;
 	
-	x.iloc[[row_1, row_2]] = x.iloc[[row_2, row_1]].values
-	print(f"\tR{row_1 + 1} <-> R{row_2 + 1}")
+	x.iloc[[row_a, row_b]] = x.iloc[[row_b, row_a]].values
+	print(f"\tR{row_a + 1} <-> R{row_b + 1}")
 	minimal_print_df(df)
 	# no return val cuz df are like pointers
 
 def row_add(
 		x:pd.DataFrame,
-		row_1:int = 1,
-		row_2:int = 1,
-		row_1_multiple:int = 1,
-		row_2_multiple:int = 1,
+		row_a:int = 1,
+		row_b:int = 1,
+		row_a_multiple:int = 1,
+		row_b_multiple:int = 1,
 		is_add:bool = True 
 	) -> None:
 	# the changed row will be on row 1
 	
-	row_1 = row_1 - 1
-	row_2 = row_2 - 1
+	row_a = row_a - 1
+	row_b = row_b - 1
 	# because math starts with 1 ;^;
 		
-	temp_row_2:pd.Series = x.iloc[row_2] * row_2_multiple
+	temp_row_b:pd.Series = x.iloc[row_b] * row_b_multiple
 	
-	x.iloc[row_1] = x.iloc[row_1] * row_1_multiple
+	x.iloc[row_a] = x.iloc[row_a] * row_a_multiple
 	if is_add:
-		x.iloc[row_1] = x.iloc[row_1] + temp_row_2
+		x.iloc[row_a] = x.iloc[row_a] + temp_row_b
 	else:
-		x.iloc[row_1] = x.iloc[row_1] - temp_row_2
+		x.iloc[row_a] = x.iloc[row_a] - temp_row_b
 	
 	# ====================================================================
 	
 	report:str = "\t"
-	report += f"{row_1_multiple}R{row_1+1}"
+	report += f"{row_a_multiple}R{row_a+1}"
 	if is_add:
 		report += " + "
 	else:
 		report += " - "
-	report += f"{row_2_multiple}R{row_2+1}"
-	report += f" -> R{row_1+1}"
+	report += f"{row_b_multiple}R{row_b+1}"
+	report += f" -> R{row_a+1}"
 	
 	print(report)
 	minimal_print_df(df)
 
 def row_multiply(
 		x:pd.DataFrame, 
-		row:int = 1, 
+		row_number:int = 1, 
 		row_multiple: int = 1,
 		is_multiply:bool = True
 	) -> None:
 
-	row = row - 1
+	row_number = row_number - 1
 	if is_multiply:
-		x.iloc[row] = x.iloc[row] * row_multiple
+		x.iloc[row_number] = x.iloc[row_number] * row_multiple
 	else:
-		x.iloc[row] = x.iloc[row] / row_multiple
+		x.iloc[row_number] = x.iloc[row_number] / row_multiple
 		
 	# ====================================================================
 	
 	report:str = "\t"
-	report += f"R{row+1}"
+	report += f"R{row_number+1}"
 	if is_multiply:
 		report += " * "
 	else:
 		report += " / "
 	report += f"{row_multiple}"
-	report += f" -> R{row+1}"
+	report += f" -> R{row_number+1}"
 	
 	print(report)
 	minimal_print_df(x)
